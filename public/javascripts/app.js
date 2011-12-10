462d1ea32dadd5dfd63c207a21e9ca4836a82beb
Patrick Forringer <patrick@forringer.com>
2011-12-10 01:09:23 -0600

    Passing data along with the image, and a little bit of styling

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,14 @@
-var socket = io.connect('http://localhost:4000');
-socket.on('images', function(data){
-  data.forEach(function(image){
-    var img = $("<img />", {src:image});
-    $("#images").append(img);
-    $("#images").append(image);
+
+jQuery(function($) {
+  
+  var socket = io.connect('http://localhost:4000');
+  
+  var image_template = Handlebars.compile($('#image').html());
+  
+  socket.on('images', function(data){
+    data.forEach(function(image){
+      $("#images").append(image_template(image));
+    });
   });
+
 });
-
-
