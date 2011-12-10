05e25ef9a937bc498685579fa4555fa7ed8a71c6
BuddyL <buddy@buddylindsey.com>
2011-12-09 23:42:51 -0600

    change url

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('http://evening-light-2225.herokuapp.com/');
 socket.on('images', function(data){
   data.forEach(function(image){
     var img = $("<img />", {src:image});
